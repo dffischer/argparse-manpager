c357755e2109e2f3610044382fcb59bb5d3aabc2
XZS <d.f.fischer@web.de>
2015-01-01 21:07:05 +0100

    find local imports as dependencies

--- waftools/manpyger.py
+++ waftools/manpyger.py
@@ -1,6 +1,7 @@
 from waflib.Task import Task
 from waflib.TaskGen import feature
 from waflib.Utils import O755, subst_vars, to_list
+from re import compile, MULTILINE
 
 def options(ctx):
     ctx.load('python gnu_dirs')
@@ -18,8 +19,31 @@ class entrypynt(Task):
                 subst_vars("exec ${PYTHON} -m ${MODULE} $@", self.env))
         starter.chmod(O755)
 
+def find_py(location, module, entry="__init__"):
+    """find a python module or entry point"""
+    module = module.replace(".", "/")
+    result = location.find_node(module)
+    if result:
+        return result.find_node(entry + ".py")
+    else:
+        return location.find_node(module + ".py")
+
 class manpyge(Task):
     run_str = "PYTHONPATH=${gen.path.abspath()}: ${PYTHON} -Bm manpager ${MODULE} > ${TGT}"
+
+    def scan(self, imp=compile("^from (\..+) import .+$|^import (\..+)$", MULTILINE)):
+        """find local imports recursively"""
+        module = find_py(self.generator.path, self.env.MODULE, "__main__")
+        unseen = {module}
+        seen = set()
+        while unseen:
+            module = unseen.pop()
+            seen.add(module)
+            for match in imp.finditer(module.read()):
+                next = find_py(module.parent, match.group(1)[1:])
+                if next not in seen:
+                    unseen.add(next)
+        return seen, None
 
     def keyword(self):
         return "Documenting module"
